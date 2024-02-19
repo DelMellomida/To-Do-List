3386f5cdf317ab43ce8836894d49a043be2be778
Jhondel Mellomida <160392395+DelMellomida@users.noreply.github.com>
2024-02-20 00:04:45 +0800

    Update signup_model.inc.php

--- includes/signup_model.inc.php
+++ includes/signup_model.inc.php
@@ -21,3 +21,20 @@ function get_email(object $pdo, string $email) {
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     return $result;
 }
+
+function set_user(object $pdo, string $name, string $email, string $username, string $password) {
+    $query = "INSERT INTO userInfo(name, email, username, password) VALUES (:name, :email, :username, :password);";
+    $stmt = $pdo->prepare($query);
+
+    $options = [
+        'cost' => 12
+    ];
+
+    $hashedPwd = password_hash($password, PASSWORD_BCRYPT, $options);
+
+    $stmt->bindParam(":name", $name);
+    $stmt->bindParam(":email", $email);
+    $stmt->bindParam(":username", $username);
+    $stmt->bindParam(":password", $hashedPwd);
+    $stmt->execute();
+}
